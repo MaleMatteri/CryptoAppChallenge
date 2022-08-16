15a4740b22a0d88b2870570016970067de6275d3
Male Matteri <mjmatteri@gmail.com>
2022-08-16 12:43:37 -0300

    Se comienza el diseño de la tercer pantalla, agregando los elementos principales.

--- lib/add_new_crypto.dart
+++ lib/add_new_crypto.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'crypto_search_list.dart';
 
 class ThridRoute extends StatelessWidget {
   const ThridRoute({Key? key}) : super(key: key);
@@ -10,12 +11,46 @@ class ThridRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Add a new fav crypto'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "NombreDeLaCrypto",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter the quantity",
+                prefixIcon: Icon(Icons.paid),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Respond to button press
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(150, 60),
+              ),
+              icon: Icon(Icons.save, size: 25),
+              label: Text("Save", style: TextStyle(fontSize: 20.0)),
+            )
+          ],
         ),
       ),
     );
